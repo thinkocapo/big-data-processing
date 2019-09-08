24671978c6f85da41dde38663ca2dfebcb94c1eb
William Capozzoli <thinkocapo@gmail.com>
2019-09-07 22:42:14 -0700

    works

--- hw1/problem_1.py
+++ hw1/problem_1.py
@@ -1,27 +1,50 @@
 import inspect
 from random import randint
+import _thread
 import threading
 
+threads = []
 
 def text():
     output = '%s %s %s\n' % (randint(0,10), randint(0,10), randint(0,10))
     return output
 
-# wrap this (decorate?) in its own thread execution
 def create_file(numLines, num):
-    # wrap this part in its own thread execution
+    print('create_file %s' % (num))
     file = open("william_capozzoli_{}.txt".format(num),"w+")
     for numLine in range(numLines):
         file.write(text())
 
-def generate_file(numFiles, numLines):
+def generate_files(numFiles, numLines):
     """numFiles to generate. numLines per file"""
     print('generate_file')
     for num in range(numFiles):
-        create_file(numLines, num)
-    print('\nDONE')
+        print('num %s' % (num))
+        thread = threading.Thread(target=create_file,args=(numLines, num))
+        threads.append(thread)
 
+def run_the_threads():
+    for thread in threads:
+        thread.start()
+
+    # Wait for all to complete
+    for thread in threads:
+        thread.join()
 
 
 if __name__ == '__main__':
-    generate_file(2, 3)
+    generate_files(2, 3)
+    run_the_threads()
+
+
+# experiment 1
+# def thread_decorator(func):
+#     def wrapper():
+#         print("OPEN A THREAD")
+#         func() # create_file inside of it
+#         print("THREAD CLOSED?")
+#     return wrapper
+# generate_file = thread_decorator(generate_file)
+
+# experiment 2
+# instead of Decorator, could do a Class that inherits from threading.Thread, like https://www.geeksforgeeks.org/writing-files-background-python/
