65ed77f7e0323f48c4e95a24cfa553eda7500524
William Capozzoli <thinkocapo@gmail.com>
2019-09-12 20:30:37 -0700

    program and number of threads, function

--- assignment2/processor.py
+++ assignment2/processor.py
@@ -1,22 +1,29 @@
 import argparse
 
+def io_intensive(numThreads):
+    print('io_intensive')
 
-def io_processor():
-    print('io_processor')
+def cpu_intensive(numThreads):
+    print('cpu_intensive')
 
-def cpu_processor():
-    print('cpu_processor')
+def main():
+    # specify the number of threads and program from command-line
+    parser = argparse.ArgumentParser()
+    parser.add_argument("numThreads", type=int, help="numThreads")
+    parser.add_argument("program", type=str, help="io_intensive or cpu_intensive")
+    args = parser.parse_args()
+    print(args)
 
-def program(func):
-    func()
+    # which program are we calling
+    programs={'io_intensive': io_intensive, 'cpu_intensive': cpu_intensive}
+    program = programs[args.program]
 
+    # and with how many threads
+    numThreads = args.numThreads
+    
+    program(numThreads)
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--threads", "-t", type="int", help="numThreads")
-    args = parser.parse_args()
-    print(args.echo)
-    # func = io or cpi
-    program()
+    main()
 else:
     print('this is a main level package')
