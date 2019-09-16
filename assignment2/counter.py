dc422f526a2151f64d6e0db2fc77f19e23bd8135
William Capozzoli <thinkocapo@gmail.com>
2019-09-15 20:44:12 -0700

    shared memory Manager

--- assignment2/counter.py
+++ assignment2/counter.py
@@ -4,6 +4,7 @@ import multiprocessing
 import os
 import time
 import threading
+from datetime import datetime
 from random import randint
 
 # Capture any exceptions and send to Sentry.io :)
@@ -12,17 +13,20 @@ if 'DSN_DATA_PIPELINE' in os.environ:
     sentry_sdk.init(os.environ['DSN_DATA_PIPELINE'])
 
 processes = []
-# <uuid> <timestamp> <url> <userId> <country> <ua_browser><ua_os> <response_status> <TTFB> 
-fieldNames = ['uuid', 'timestamp', 'url', 'userId', 'country', 'ua_browser', 'ua_os', 'response_status', 'TTFB']
+field_names = ['uuid', 'timestamp', 'url', 'userId', 'country', 'ua_browser', 'ua_os', 'response_status', 'TTFB']
 
-
-
-def query1(fileName, output):
-    input_file = csv.DictReader(open(fileName), fieldnames=fieldNames)
+def query1(fileName, shared_memory):
+    input_file = csv.DictReader(open(fileName), fieldnames=field_names)
     for row in input_file:
-        if row['data
-        ']
-        print row
+        timestamp_str = row['timestamp']
+        obj = datetime.strptime(timestamp_str, '%Y-%m-%dT%H:%M:%S.%fZ')
+        timestamp_output = '%s-%s-%s:%s' % (obj.year, obj.month, obj.day, obj.hour)
+        if timestamp_output not in shared_memory:
+            shared_memory[timestamp_output] = 1
+        else:
+            shared_memory[timestamp_output] =+ 1
+
+
 
 def query2(file):
     print('query2')
@@ -46,15 +50,15 @@ def main():
     fileNames = ('./input_files/file-input1.csv', 'input_files/file-input2.csv', 'input_files/file-input3.csv', 'input_files/file-input4.csv')
 
     with multiprocessing.Manager() as manager:
-        output = manager.dict()
+        shared_memory = manager.dict()
         for i in range(4):
-            process = multiprocessing.Process(target=query, args=(fileNames[0], output,))
+            process = multiprocessing.Process(target=query, args=(fileNames[0], shared_memory,))
             processes.append(process)
             process.start()
             # process.join()
         for curr_process in processes:
             curr_process.join()
-    print('output\n{}'.format(output))
+    print('shared_memory\n{}'.format(shared_memory))
 
 
 
@@ -93,3 +97,23 @@ else:
 # reader = csv.reader(open(fileName), delimiter=' ', quotechar='|')
 # for row in reader:
 #     print(', '.join(row))
+
+
+
+
+    
+# obj_3 = datetime.strptime(log_date, '%Y-%m-%dT%H:%M:%S.%f')    
+#                 '2019-09-12T19:53:11.040Z'
+# date_time_str = '2018-06-29 08:15:27.243860'
+# date_time_obj = datetime.datetime.strptime(date_time_str, '%Y-%m-%d %H:%M:%S.%f')
+
+
+# https://stackabuse.com/converting-strings-to-datetime-in-python/
+# log_date = '2019-09-12T19:53:11.040Z'
+# formatted = datetime.strptime(log_date, '%Y-%m-%dT%H:%M:%S.%fZ') 
+
+
+# formatted.date()
+# formatted.time()
+# formatte.year
+# formatted.hour
