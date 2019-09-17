b3b8639fc578b7b759bb58d9d0dc27e3ba16b134
William Capozzoli <thinkocapo@gmail.com>
2019-09-16 19:08:23 -0700

    works

--- assignment2/counter.py
+++ assignment2/counter.py
@@ -17,46 +17,90 @@ if 'DSN_DATA_PIPELINE' in os.environ:
 
 field_names = ['uuid', 'timestamp', 'url', 'userId', 'country', 'ua_browser', 'ua_os', 'response_status', 'TTFB']
 
+# Makes a data structure like { date1: { url:count } }
 def query1(lock, fileName, server_process_dict):
     input_file = csv.DictReader(open(fileName), fieldnames=field_names)
     for row in input_file:
-        timestamp_str = row['timestamp']
+        timestamp = row['timestamp']
         url = row['url']
         obj = datetime.time()
         try:
-            obj = datetime.datetime.strptime(timestamp_str, '%Y-%m-%dT%H:%M:%S.%fZ')
+            obj = datetime.datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S.%fZ')
         except ValueError: 
-            obj = datetime.datetime.strptime(timestamp_str, '%Y-%m-%dT%H:%M:%SZ')
+            obj = datetime.datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%SZ')
         timestamp_key = '%s-%s-%s:%s' % (obj.year, obj.month, obj.day, obj.hour)
-        # dict_copy = server_process_dict[timestamp_key]
-        # dict_copy[url] = dict_copy[url] + 1
-        # server_process_dict[timestamp_key] = dict_copy
         with lock:
             if timestamp_key in server_process_dict:
-                dict_copy = server_process_dict[timestamp_key]
+                url_dict = server_process_dict[timestamp_key]
                 if url in server_process_dict[timestamp_key]:
-                    dict_copy[url] = dict_copy[url] + 1
-                    server_process_dict[timestamp_key] = dict_copy
+                    url_dict[url] = url_dict[url] + 1
+                    server_process_dict[timestamp_key] = url_dict
                 else:
-                    dict_copy[url] = 1
-                    server_process_dict[timestamp_key] = dict_copy
+                    url_dict[url] = 1
+                    server_process_dict[timestamp_key] = url_dict
             else:
                 server_process_dict[timestamp_key] = { url: 1 }
 
-def query2(file):
-    print('query2')
+# Makes a data structure like { timestamp: { url: [users] }}
+def query2(lock, fileName, server_process_dict):
+    input_file = csv.DictReader(open(fileName), fieldnames=field_names)
+    for row in input_file:
+        timestamp = row['timestamp']
+        url = row['url']
+        userId = row['userId']
+        obj = datetime.time()
+        try:
+            obj = datetime.datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S.%fZ')
+        except ValueError: 
+            obj = datetime.datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%SZ')
+        timestamp_key = '%s-%s-%s:%s' % (obj.year, obj.month, obj.day, obj.hour)
+
+        with lock:
+            if timestamp_key in server_process_dict:
+                url_dict = server_process_dict[timestamp_key]
+                if url in url_dict:
+                    url_dict[url].append(userId)
+                    server_process_dict[timestamp_key] = url_dict
+                else:
+                    url_dict[url] = [userId]
+                    server_process_dict[timestamp_key] = url_dict
+            else:
+                server_process_dict[timestamp_key] = { url: [userId] }
+
+# Makes a data structure that looks like:
+def query3(lock, fileName, server_process_dict):
+    input_file = csv.DictReader(open(fileName), fieldnames=field_names)
+    for row in input_file:
+        timestamp = row['timestamp']
+        url = row['url']
+        userId = row['userId']
+        obj = datetime.time()
+        try:
+            obj = datetime.datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S.%fZ')
+        except ValueError: 
+            obj = datetime.datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%SZ')
+        timestamp_key = '%s-%s-%s:%s' % (obj.year, obj.month, obj.day, obj.hour)
+
+        with lock:
+            print 'yo'
 
 def printer(query):
+    # Print the number of unique URL's per query
     if query == query1:
         for k,v in server_process_dict.items():
             unique_urls = server_process_dict[k].items()
-            count_unique_urls = len(unique_urls)
-            print k, count_unique_urls
+            print k, len(unique_urls)
+    # Print the number of unique visitors per URL per day
+    if query == query2:
+        for timestamp_key, url_dict in server_process_dict.items():
+            for url, user_list in url_dict.items():
+                unique_users = set(user for user in user_list)
+                print timestamp_key, url, len(unique_users)
 
-# Example usage:
+# EXAMPLE USAGE:
 # python3 countery.py query1
+# python3 countery.py query2
 if __name__ == '__main__':
-    print('main')
 
     # Specify the number of threads and query from command-line
     parser = argparse.ArgumentParser()
@@ -81,21 +125,3 @@ if __name__ == '__main__':
             curr_process.join()
 
         printer(query)
-        # print(server_process_dict.items())
-        # for k,v in server_process_dict.items():
-        #     unique_urls = server_process_dict[k].items()
-        #     count_unique_urls = len(unique_urls)
-        #     print k, count_unique_urls
-
-
-# if full_key in server_process_dict:
-#     server_process_dict[full_key] += 1    
-# else:
-#     server_process_dict[full_key] = 1   
-
-# if timestamp_key in server_process_dict:
-#     server_process_dict[timestamp_key].append(url)
-# else:
-#     server_process_dict[timestamp_key] = [url]
-
-# full_key = '%s %s' % (timestamp_key, url)
