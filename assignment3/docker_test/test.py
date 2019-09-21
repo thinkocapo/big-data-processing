d709c8c048214d0b2899093282ec49960e8f9bbd
William Capozzoli <thinkocapo@gmail.com>
2019-09-21 16:18:08 -0700

    IPv4

--- assignment3/docker_test/test.py
+++ assignment3/docker_test/test.py
@@ -1,1 +1,15 @@
-print('HELLO EARTH')
+import argparse
+import csv
+import datetime
+import redis
+
+print('HELLO RESIDENT EARTH')
+
+
+# Create a redis client
+# docker network inspect bridge | grep IPv4
+redisClient = redis.StrictRedis(host='172.17.0.2',port=6379)
+redisClient.set("myfoo", "yourbar")
+
+
+print('FINISHED LANDING OPERATIONS')
