4f234cb84d45ddede1f86fb90e236c6903f1059e
William Capozzoli <thinkocapo@gmail.com>
2019-09-20 19:06:09 -0700

    unique visitors per url per hour

--- assignment3/redis-client.py
+++ assignment3/redis-client.py
@@ -11,6 +11,8 @@ parser.add_argument('--logs-directory', '-ld', required=False, help="Directory w
 parser.add_argument('--file_name', '-file', required=False, help="Name of the  log file are stored")
 parser.add_argument("query", type=str, , required=True, help="query1 query2 query3")
 
+field_names = ['uuid', 'timestamp', 'url', 'userId', 'country', 'ua_browser', 'ua_os', 'response_status', 'TTFB']
+
 # Calculate the unique url's per hour - per hour of every yy/mm/dd/hh?
 # https://redis.io/commands/smembers
 def query1_unique_urls_per_hour(file_name):
@@ -28,16 +30,19 @@ def query1_unique_urls_per_hour(file_name):
         timestamp_hour = '%s-%s-%s:%s' % (date.year, date.month, date.day, date.hour)
 
         # Write to Redis
-        # simplest...
+        # 2 python perations (if..not..in and url_map), 1 redis operation
         if url not in url_map:
             url_map[url] = True
             redisClient.hincrby(timestamp_hour, 'count' 1)
         
-        # less python, but much more redis, and a strange data structure?
-        if redisClient.hget(timestamp_hour, url) == None:
-            redisClient.hset(timestamp_hour, url, 'true')
-            redisClient.hincrby(timestamp_hour, 'count' 1)
+        # 0 python operations, but is 3 redis operations (get, set and increment), and a strange data structure?
+        # if redisClient.hget(timestamp_hour, url) == None:
+        #     redisClient.hset(timestamp_hour, url, 'true')
+        #     redisClient.hincrby(timestamp_hour, 'count' 1)
         
+        # redis-cli
+        # HGET 2019-09-20:12 count
+
     return
 
 def query2_unique_visitors_per_url_per_hour(file_name):
@@ -47,6 +52,7 @@ def query2_unique_visitors_per_url_per_hour(file_name):
         timestamp = row['timestamp']
         url = row['url']
         date = datetime.time()
+        userId = row['userId']
         try:
             date = datetime.datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S.%fZ')
         except ValueError: 
@@ -55,12 +61,19 @@ def query2_unique_visitors_per_url_per_hour(file_name):
         timestamp_hour = '%s-%s-%s:%s' % (date.year, date.month, date.day, date.hour)
 
         # Write to Redis
-        # "key_name, score1, value"
-        # "key_name, score2, value"
-        # Insert a new key with initial value 1. If that key already exists, then the function returns 0
-        #if redisClient.hsetnx(timestamp_hour,url,1) == 0:
-            # Atomically increment the value for key by 1 if the key already exists
-            #redisClient.hincrby(timestamp_hour,url)
+        full_key = '%s:%s' % (timestamp_hour, url)
+        if redisClient.hget(full_key, userId) == None:
+            redisClient.hset(full_key, userId, 'true')
+            redisClient.hincrby(full_key, 'count' 1)
+    return
+    '''
+    <date:hour:url>,  unique_user_count
+    2019-09-14:14:http://example.com/?url=042, ??
+    2019-09-12:19:http://example.com/?url=013, ??
+    2019-09-14:03:http://example.com/?url=162, ??
+    2019-09-13:01:http://example.com/?url=035, ??
+    2019-09-14:10:http://example.com/?url=043, ??
+    '''
 
 
 def query3_unique_events_per_url_per_hour(file_name):
