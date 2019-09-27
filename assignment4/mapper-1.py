58e562d25feb73865e706c3d651947efeee38dc9
William Capozzoli <thinkocapo@gmail.com>
2019-09-26 23:30:02 -0700

    right

--- assignment4/mapper-1.py
+++ assignment4/mapper-1.py
@@ -1,50 +1,55 @@
 #!/usr/bin/python3
-# import argparse
 import csv
 import datetime
 import sys
 
 # hadoop jar /usr/lib/hadoop-mapreduce/hadoop-mapreduce-examples.jar wordcount s3a://inputfilesassignment4/file-input1.csv s3a://inputfilesassignment4/wordcount
-'''
-hadoop jar /usr/lib/hadoop/hadoop-streaming.jar -file ./mapper-1.py -mapper ./mapper-1.py -file ./reducer-1.py -reducer
-./reducer-1.py -input inputfilesassignment4/ -output wordcount1
-
-hadoop jar /usr/lib/hadoop/hadoop-streaming.jar \
-    -file ./mapper-1.py \
-    -mapper ./mapper-1.py \ 
-    -file ./reducer-1.py \
-    -reducer ./reducer-1.py \
-    -input s3a://inputfilesassignment4/ \
-    -output s3a://inputfilesassignment4/wc1
-'''
-
-field_names = ['uuid', 'timestamp', 'url', 'userId', 'country', 'ua_browser', 'ua_os', 'response_status', 'TTFB']
-
 
 # Unique URLs per Hour
-for line in sys.stdin:
-    # TRY THIS
-    # fields = line.split(",")
+for line_in in sys.stdin:
 
-    # TRY THIS
-    # for col in line
-
-    timestamp = line[1]
-    url = line[2]
-    date = datetime.time()
-    
-    # Prepare a key in the form of <timestamp>:<hour>
     try:
-        date = datetime.datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S.%fZ')
-    except ValueError: 
-        date = datetime.datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%SZ')
-    timestamp_hour = '%s-%s-%s:%s' % (date.year, date.month, date.day, date.hour)
+        line = line_in.split(",")
 
-    # Prepare a key
-    timestamp_hour_url = '%s-%s' % (timestamp_hour, url)
+        # TRY THIS
+        # for col in line
 
-    # Output, stdout
-    print('%s\t%s' % (timestamp_hour_url, 1))
+        # timestamp = line[1]
+        # url = line[2]
+
+        timestamp = line[1]
+        url = line[2]
+
+        date = datetime.time()
+        
+        # Prepare a key in the form of <timestamp>:<hour>
+        try:
+            date = datetime.datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S.%fZ')
+        except ValueError: 
+            date = datetime.datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%SZ')
+
+        day = date.day
+        if len(str(day)) == 1:
+            day = '0{}'.format(day)
+        month = date.month
+        if len(str(month)) == 1:
+            month = '0{}'.format(month)
+        hour = date.hour
+        if len(str(hour)) == 1:
+            hour = '0{}'.format(hour)
+
+        timestamp_hour = '%s-%s-%s:%s' % (date.year, month, day, hour)
+
+        # Prepare a key
+        timestamp_hour_url = '%s-%s' % (timestamp_hour, url)
+
+        # Output, stdout
+        # print('%s\t%d' % (timestamp_hour_url, 1))
+        print('%s\t%d' % (timestamp_hour, 1))
+
+
+    except ValueError as e:
+        continue
 
 
 # print("\nMapper-1 Process Completed")
--- assignment4/reducer-1.py
+++ assignment4/reducer-1.py
@@ -6,24 +6,43 @@ import fileinput
 current_timestampHour_url = None
 current_count = 1
 
+timestampHour_url_dict = {}
+timestampHour_dict = {}
+
 for line in sys.stdin:
     try:
-        timestampHour_url, count = line.strip().split('\t')
+        timestampHour, count = line.strip().split('\t')
+        if timestampHour in timestampHour_dict:
+            timestampHour_dict[timestampHour] = timestampHour_dict[timestampHour] + 1
+        else:
+            timestampHour_dict[timestampHour] = 1
 
-        # if it's sorted then it will match
-        if current_timestampHour_url:
-            if timestampHour_url == current_timestampHour_url:
-                current_count += int(count)
-            else:
-                # Output
-                print ("%s\t%d" % (current_timestampHour_url, current_count))
-                current_count = 1
 
-        current_timestampHour_url = timestampHour_url
+        # timestampHour_url, count = line.strip().split('\t')
+
+        # if timestampHour_url in timestampHour_url_dict:
+        #     timestampHour_url_dict[timestampHour_url] = timestampHour_url_dict[timestampHour_url] + 1
+        # else:
+        #     timestampHour_url_dict[timestampHour_url] = 1
 
     except ValueError as e:
         continue
-        
-if current_count > 1:
-    # Output
-    print ("%s\t%d" % (current_timestampHour_url, current_count))
+
+    for timestampHour in timestampHour_dict:
+        count = timestampHour_dict[timestampHour]
+        print("%s\t%s" % (timestampHour, count))
+
+# ?????????
+# if current_count > 1:
+#     # Output
+#     print ("%s\t%d" % (current_timestampHour_url, current_count))
+
+# if it's sorted then it will match
+# if current_timestampHour_url:
+#     if timestampHour_url == current_timestampHour_url:
+#         current_count += int(count)
+#     else:
+#         # Output
+#         print ("%s\t%d" % (current_timestampHour_url, current_count))
+#         current_count = 1
+# current_timestampHour_url = timestampHour_url
