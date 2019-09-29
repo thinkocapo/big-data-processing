0e25beb7cce2120acf1953d7db609bb964b97ad1
William Capozzoli <thinkocapo@gmail.com>
2019-09-29 17:27:51 -0500

    format mappers reducers 2

--- assignment4/reducer2.py
+++ assignment4/reducer2.py
@@ -1,7 +1,7 @@
-#!/Library/Frameworks/Python.framework/Versions/3.7/bin/python3
+#!/usr/bin/python3
+#/Library/Frameworks/Python.framework/Versions/3.7/bin/python3
 
 import sys
-
 current_date_hour_url = None
 date_hour_url = None
 _dict = {}
@@ -18,15 +18,19 @@ for line in sys.stdin:
         
         if current_date_hour_url:
             if date_hour_url == current_date_hour_url:
-                if date_hour_url in _dict: 
+                if date_hour_url in _dict:
+                    # If user already seen for this date_hour_url then increment
                     if user in _dict[date_hour_url]:
                         _dict[date_hour_url][user] += 1
+                    # it's a new user for this date_hour_url
                     else:
                         _dict[date_hour_url][user] = 1
                 else:
+                    # first time reducer sees this date_hour_url
                     _dict[date_hour_url] = {}
                     _dict[date_hour_url][user] = 1
-            else:        
+            else:
+                # no more of the current_date_hour_url in the sorted input coming in, so print   
                 if current_date_hour_url in _dict:
                     print ("%s\t%d" % (current_date_hour_url, len(_dict[current_date_hour_url].keys())))
                 _dict = {}
