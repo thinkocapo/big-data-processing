f7b39c33aad0d0f9962c0cd6d91d4a9a5ef748e6
William Capozzoli <thinkocapo@gmail.com>
2019-09-29 17:46:52 -0500

    query3

--- assignment4/mapper3.py
+++ assignment4/mapper3.py
@@ -1,1 +1,44 @@
 #!/usr/bin/python3
+
+import datetime
+import sys
+
+# Query2: get count of unique visitors per URL per hour
+import fileinput
+# for line_in in sys.stdin:
+for line in fileinput.input():
+
+    try:
+        # Prepare the timestamp and url in python
+        line = line.split(",")
+        timestamp = line[1]
+        url = line[2]
+        user = line[3]
+
+        # Prepare a Date object from parsed csv timestamp
+        date = datetime.time()
+        try:
+            date = datetime.datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S.%fZ')
+        except ValueError: 
+            date = datetime.datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%SZ')
+
+        # Ensure that day and month are always 2-digits instead of 1-digit
+        day = date.day
+        if len(str(day)) == 1:
+            day = '0{}'.format(day)
+        month = date.month
+        if len(str(month)) == 1:
+            month = '0{}'.format(month)
+        hour = date.hour
+        if len(str(hour)) == 1:
+            hour = '0{}'.format(hour)
+
+        # Prepare a key in form <timestamp>:<hour>
+        timestamp_hour = '%s-%s-%s-%s' % (date.year, month, day, hour)
+        timestamp_hour_url = '%s_%s' % (timestamp_hour, url)
+  
+        # K,V Output as stdout in form <YYYY-MM-DD-HH, 1>
+        print('%s\t%d' % (timestamp_hour_url, 1))
+
+    except ValueError as e:
+        continue
