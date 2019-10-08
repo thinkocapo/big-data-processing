8cd3fb02f4be540e762a01648585f4bf6a5b7a29
William Capozzoli <thinkocapo@gmail.com>
2019-10-07 22:48:47 -0700

    updates

--- assignment5/problem2.py
+++ assignment5/problem2.py
@@ -2,7 +2,7 @@ import sys
 import datetime
 from pyspark import SparkContext
 from pyspark.sql import SparkSession
-
+from pyspark.sql.types import *
 
 if __name__ == "__main__":
 
@@ -16,20 +16,18 @@ if __name__ == "__main__":
 
     # Create a dataframe from the csv
     schema = StructType([
-        StructField('uniqueID', StringType(), True),
+        StructField('uuid', StringType(), True),
         StructField('timestamp', TimestampType(), True),
         StructField('url', StringType(), True),
         StructField('user', StringType(), True),
         StructField('country', StringType(), True),
     ])
-    # ORIGINAL - works
-    # df = spark.read.load("s3a://inputfilesassignment4/wordcount/", format="csv").schema(schema)
-    df = spark.read.format(csv).schema(schema).load("s3a://inputfilesassignment4/wordcount/")
-    works
-    count = df.count()
-    print('\n~~~~~~~~~ count {}'.format(count))
-    first = df.head()
-    print('\n~~~~~~~~~ first {}'.format(first))
+    
+    # ORIGINAL - no schema
+    # df = spark.read.load("s3a://inputfilesassignment4/wordcount/", format="csv")
+    
+    # UPDATED - has schema
+    df = spark.read.format('csv').schema(schema).load("s3a://inputfilesassignment4/wordcount/")
 
     # Write the datagrame as a parquet in HDFS
     df.write.parquet('hdfs:///user/hadoop/parquet2')
--- assignment5/problem3.py
+++ assignment5/problem3.py
@@ -1,14 +1,12 @@
-'''
-P2 and P3 - data frames. 
-Since we did not mention it explicitly - you can use SQL as well, but try to use data frames to get a more versatile experience
-'''
-
 import sys
 import datetime
 from pyspark import SparkContext
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
 
-
+'''
+unique_urls_by_country_by_hour_for_time_range
+'''
 if __name__ == "__main__":
 
     # Create a SparkContext and a Spark Session
@@ -18,14 +16,9 @@ if __name__ == "__main__":
         .appName("Python Spark SQL basic example") \
         .config("spark.some.config.option", "some-value") \
         .getOrCreate()
+    df_parquet = spark.read.parquet('hdfs:///user/hadoop/parquet2')
 
-    df_parquet_no_schema = spark.read.parquet('hdfs:///user/hadoop/parquet1')
-    print('11111111111')
-    parts = df_parquet_no_schema.rdd.map(lambda l: l.split(","))
-    # turn timestamp into day/hour
-    dateHour_country = parts.map(lambda p: Row(timestamp=p[1],url=p[2],user=p[3],country=p[4]))
-    df_parquet = spark.createDataFrame(dateHour_country)
-    print('22222222222')
+    # WRONG - need date:hour instead of timestamp, at least according to classmate's example.
     df_parquet.groupBy('timestamp', 'country')\
         .agg(countDistinct('url'))\
         .orderBy('timestamp', 'country')\
@@ -33,17 +26,14 @@ if __name__ == "__main__":
 
     # ORIGINAL 
     # df_parquet = spark.read.load('hdfs:///user/hadoop/parquet1')
-    # works
+    
+    # UPDATED
     count = df_parquet.count()
-    print('\n~~~~~~~~~ count {}'.format(count))
+    print('\n++++++ Count {}'.format(count))
     first = df_parquet.head()
-    print('\n~~~~~~~~~ first {}'.format(first))
+    print('\n++++++ First {}'.format(first))
+
+    for item in df_parquet.rdd.collect():
+        print(item)
 
     sc.stop()
-
-
-    # ORIGINAL
-    # df_parquet.groupBy('day:hour', 'country')\
-    #     .agg(countDistinct('url'))\
-    #     .orderBy('hour', 'country')\
-    #     .collect()
