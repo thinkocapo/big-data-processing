ad0232baa31b6ab2a51240e05af115a107a75a78
William Capozzoli <thinkocapo@gmail.com>
2019-10-20 12:10:36 -0700

    subprocess call

--- assignment7/problem2/curl.py
+++ assignment7/problem2/curl.py
@@ -1,15 +1,29 @@
 import argparse
+import os
+import subprocess
+import time
 
 # Argument Parser
 prog = "assignment3"
-desc = "Process data provided by log files and return query results"
-parser = argparse.ArgumentParser(prog=prog, description=desc)
+parser = argparse.ArgumentParser(prog=prog)
 parser.add_argument("rate", type=str, help="rate, interval")
 args = parser.parse_args()
 
+# Using your curl python example, in the interest of time, getting all problems done:
+# Writes to DEVNULL because we don't actually need the response. Flume will get it from log file via 'tail
 def main():
-    print('do things {}'.format(args.rate))
+    rate = int(args.rate)
+    wait_time = 1.0 / rate
+    print('wait_time {} \n'.format(wait_time))
 
+    count = 0
+    with open(os.devnull, 'w') as DEVNULL:
+        while True:
+            subprocess.call(['curl', '-s', 'http://ec2-18-220-93-94.us-east-2.compute.amazonaws.com:80'], stdout=DEVNULL)
+            count += 1
+            if count % rate == 0:
+                print('count {} \n'.format(count))
+            time.sleep(wait_time)
 
 
 
